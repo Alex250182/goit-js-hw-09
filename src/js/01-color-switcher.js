46dc9913adda194661500c7563e07e72f83fa14c
Alex250182 <119724458+Alex250182@users.noreply.github.com>
2023-05-21 11:26:44 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+        }
+        document.body.style.background = getRandomHexColor();
+    },1000)
+
+    btnStart.disabled = true;
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId)
+
+    btnStart.disabled = false;
+})
